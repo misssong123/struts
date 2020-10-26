6359f50f4079730c99b04f7dc93a6c3359484a1d
root <root@qq.com>
2020-10-26 12:45:04 +0800

    二叉树三种遍历方式

---
+++ src/com/meng/datastructures/tree/BinaryTree.java
@@ -0,0 +1,63 @@
+package com.meng.datastructures.tree;
+
+public class BinaryTree {
+    private HeroNode root;
+
+    public void setRoot(HeroNode root) {
+        this.root = root;
+    }
+    //前序遍历
+    public void preOrder() {
+        if(this.root != null) {
+            this.root.preOrder();
+        }else {
+            System.out.println("二叉树为空，无法遍历");
+        }
+    }
+
+    //中序遍历
+    public void infixOrder() {
+        if(this.root != null) {
+            this.root.infixOrder();
+        }else {
+            System.out.println("二叉树为空，无法遍历");
+        }
+    }
+    //后序遍历
+    public void postOrder() {
+        if(this.root != null) {
+            this.root.postOrder();
+        }else {
+            System.out.println("二叉树为空，无法遍历");
+        }
+    }
+
+    public static void main(String[] args) {
+        //先需要创建一颗二叉树
+        BinaryTree binaryTree = new BinaryTree();
+        //创建需要的结点
+        HeroNode root = new HeroNode(1, "宋江");
+        HeroNode node2 = new HeroNode(2, "吴用");
+        HeroNode node3 = new HeroNode(3, "卢俊义");
+        HeroNode node4 = new HeroNode(4, "林冲");
+        HeroNode node5 = new HeroNode(5, "关胜");
+
+        //说明，我们先手动创建该二叉树，后面我们学习递归的方式创建二叉树
+        root.setLeft(node2);
+        root.setRight(node3);
+        node3.setRight(node4);
+        node3.setLeft(node5);
+        binaryTree.setRoot(root);
+
+        //测试
+		System.out.println("前序遍历"); // 1,2,3,5,4
+		binaryTree.preOrder();
+
+        //测试
+		System.out.println("中序遍历");
+		binaryTree.infixOrder(); // 2,1,5,3,4
+
+		System.out.println("后序遍历");
+		binaryTree.postOrder(); // 2,5,4,3,1
+    }
+}
